34ca4fed742f9447c79de02ee9a2563c42b16fef
yazoo1220 <podman1220@hotmail.com>
2023-04-12 02:02:21 +0300

    Update main.py　removed for loop

--- main.py
+++ main.py
@@ -81,7 +81,5 @@ if user_input and index:
     st.session_state.generated.append(output.response)
 
 if st.session_state["generated"]:
-
-    for i in range(len(st.session_state["generated"]) - 1, -1, -1):
-        message(st.session_state["generated"][i], key=str(i))
-        message(st.session_state["past"][i], is_user=True, key=str(i) + "_user")
+    message(st.session_state["generated"], key=str(i))
+    message(st.session_state["past"], is_user=True, key=str(i) + "_user")
