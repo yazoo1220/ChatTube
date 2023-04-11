7045fb1c3e44e394fc62711702a71f6a538269c9
yazoo1220 <podman1220@hotmail.com>
2023-04-12 02:05:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -74,7 +74,9 @@ def get_text():
 
 user_input = get_text()
 
-if user_input and index:
+if !index:
+    pass
+else:
     output = index.query(user_input)
 
     st.session_state.past.append(user_input)
