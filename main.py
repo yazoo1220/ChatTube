bd3d2c1f45f272de53f605a9e3493afa5d2432a0
yazoo1220 <podman1220@hotmail.com>
2023-04-13 02:26:02 +0300

    Update main.py manually reverted to simple index mode

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import streamlit as st
 from streamlit_chat import message
 import os
 
-# show header and the authorization
+from langchain.chains import ConversationChain
+from langchain.llms import OpenAI
+
+
 st.set_page_config(page_title="ChatTube", page_icon=":robot:")
 st.header("ChatTube")
 
@@ -14,8 +17,8 @@ if "past" not in st.session_state:
     st.session_state["past"] = []
 
 
-api_token = st.text_input('OpenAI API Token',type="password")
-submit_button = st.button('authorize')
+api_token = st.text_input('OpenAI API　Token',type="password")
+submit_button = st.button('Submit')
 
 if submit_button:
     if api_token:
@@ -26,35 +29,10 @@ if submit_button:
 else:
     st.write('Waiting for API token input...')
 
-
-# set up langchain
-from langchain.chat_models import ChatOpenAI
-from langchain import PromptTemplate, LLMChain
-from langchain.prompts.chat import (
-    ChatPromptTemplate,
-    SystemMessagePromptTemplate,
-    AIMessagePromptTemplate,
-    HumanMessagePromptTemplate,
-)
-from langchain.schema import (
-    AIMessage,
-    HumanMessage,
-    SystemMessage
-)
-
-
 def load_chain():
     """Logic for loading the chain you want to use should go here."""
-    chat = ChatOpenAI(temperature=0)
-
-    template="you are a helpful assistant who can explain or summarize what the people are talking about. you should always reply by the language the user is using"
-    system_message_prompt = SystemMessagePromptTemplate.from_template(template)
-    human_template="{input}"
-    human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
-
-    chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
-
-    chain = LLMChain(llm=chat, prompt=chat_prompt)
+    llm = OpenAI(temperature=0)
+    chain = ConversationChain(llm=llm)
     return chain
 
 if os.environ['OPENAI_API_KEY']!="":
@@ -65,15 +43,9 @@ if os.environ['OPENAI_API_KEY']!="":
 else:
     st.write("waiting for api token input...")
 
-from llama_index import (download_loader,
-    GPTKeywordTableIndex,
-    LLMPredictor,
-    ServiceContext
-)
+from llama_index import download_loader, GPTSimpleVectorIndex
 
-
-# create documents for llamaindex
-video_url = st.text_input("YouTube URL")
+video_url = st.text_input("your YouTube url here")
 if video_url:
     st.video(video_url)
     YoutubeTranscriptReader = download_loader("YoutubeTranscriptReader")
@@ -85,28 +57,22 @@ else:
     loader = YoutubeTranscriptReader()
     documents = loader.load_data(ytlinks=['https://youtu.be/L_Guz73e6fw'])  
 def get_text():
-    input_text = st.text_input("You: ", "この動画の要点を3つまとめてください。回答は日本語でお願いします。", key="text")
+    input_text = st.text_input("You: ", "この動画の要点を3つまとめてください。回答は日本語でお願いします。", key="input")
     return input_text
 
 
-# interact with user and build index
 user_input = get_text()
 load_button = st.button('ask')
 
-
-from langchain.llms import OpenAI
-
+from langchain.document_loaders import YoutubeLoader
 index = ""
 if load_button:
     try:
-        llm_predictor = LLMPredictor(llm= OpenAI(temperature=0.5))
-        service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor)
-        index = GPTKeywordTableIndex.from_documents(documents, service_context=service_context)
-
+        index = GPTSimpleVectorIndex.from_documents(documents)
     except Exception as e:
         st.write("error loading the video: "+ str(e))
 else:
-    st.write("ask me anything ;)")
+    st.write("waiting for Youtube video to be loaded")
 
 
 if index == "":
