3da6e2d889e602cf31b9751f6cd6f3cfee54db44
yazoo1220 <podman1220@hotmail.com>
2023-05-11 15:01:34 +0300

    Update main.py removed llamaindex and replaced with langchain

--- main.py
+++ main.py
@@ -3,12 +3,20 @@ import streamlit as st
 from streamlit_chat import message
 import os
 
-from langchain.chains import ConversationChain
+from langchain.chains import ConversationalRetrievalChain
+from langchain.chat_models import ChatOpenAI
 from langchain.llms import OpenAI
+from langchain.document_loaders import UnstructuredURLLoader
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import Chroma
 
 
 st.set_page_config(page_title="ChatTube", page_icon=":robot:")
 st.header("▶️ ChatTube")
+
+is_gpt4 = st.checkbox('Enable GPT4',help="With this it might get slower")
+
 if "generated" not in st.session_state:
     st.session_state["generated"] = []
 
@@ -17,21 +25,26 @@ if "past" not in st.session_state:
 
 
 api_token = os.environ['OPENAI_API_KEY'] # st.text_input('OpenAI API Token',type="password")
-# submit_button = st.button('authorize')
 
-#if submit_button:
-#    if api_token:
-#        os.environ['OPENAI_API_KEY'] = api_token
-       # st.write('authorized.')
-#    else:
-#        st.write('Please input a valid API token.')
-#else:
-#    st.write('Waiting for API token...')
+def get_chat_history(inputs) -> str:
+    res = []
+    for human, ai in inputs:
+        res.append(f"Human:{human}\nAI:{ai}")
+    return "\n".join(res)
 
-def load_chain():
+def load_chain(documents):
     """Logic for loading the chain you want to use should go here."""
-    llm = OpenAI(temperature=0)
-    chain = ConversationChain(llm=llm)
+    if is_gpt4:
+        model = "gpt-4"
+    else:
+        model = "gpt-3.5-turbo"
+    llm = ChatOpenAI(temperature=0.9, model_name=model, streaming=True, verbose=True)
+    text_splitter = CharacterTextSplitter(chunk_size=3000, chunk_overlap=0)
+    docs = text_splitter.split_documents(documents)
+    embeddings = OpenAIEmbeddings()
+    db = Chroma.from_documents(docs, embeddings)
+    retriever = db.as_retriever(search_kwargs={"k": 1})
+    chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=retriever,get_chat_history=get_chat_history)
     return chain
 
 if os.environ['OPENAI_API_KEY']!="":
@@ -41,8 +54,6 @@ if os.environ['OPENAI_API_KEY']!="":
         st.write("error loading data: " + str(e))
 else:
     st.write("waiting for api token...")
-
-from llama_index import download_loader, GPTSimpleVectorIndex
 
 video_url = st.text_input("YouTube URL 🔗")
 if video_url:
@@ -64,21 +75,22 @@ user_input = get_text()
 load_button = st.button('ask')
 
 from langchain.document_loaders import YoutubeLoader
-index = ""
+qa = ""
 if load_button:
     try:
-        index = GPTSimpleVectorIndex.from_documents(documents)
+        qa = load_chain(documents)
     except Exception as e:
         st.write("error loading the video: "+ str(e))
 else:
     st.write("waiting for Youtube video to be loaded")
 
 
-if index == "":
+if qa == "":
     pass
 else:
     with st.spinner('typing...'):
-        output = index.query(user_input)
+        chat_history = []
+        result = qa({"question": prefix + user_input, "chat_history": chat_history})
         st.session_state.past.append(user_input)
         st.session_state.generated.append(output.response)
 
